58ff2b83438803990f9c9032ad7247cfb795bd6f
Wojtek Kowalczyk <wojciechkowalczyk10@gmail.com>
2025-09-09 17:25:12 +0200

    add card component with static data and shuffle it

---
+++ src/assets/boards.ts
@@ -0,0 +1,9 @@
+export const easyBoard = ['A','A','B','B','C','C','D','D'];
+export const mediumBoard = [
+  'A','A','B','B','C','C','D','D',
+  'E','E','F','F','G','G','H','H'
+];
+export const hardBoard = [
+  'A','A','B','B','C','C','D','D','E','E','F','F',
+  'G','G','H','H','I','I','J','J','K','K','L','L'
+];
