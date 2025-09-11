e6c2d7264411dc7933ec37f18adb4a5d7a8a59b4
Wojtek Kowalczyk <wojciechkowalczyk10@gmail.com>
2025-09-11 13:51:01 +0200

    add card flip logic

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import './../styles/Card.css'
+import { useState } from "react";
 
 // Interfejs w TypeScript definiuje **strukturę obiektu**, czyli jakie właściwości i ich typy powinien mieć.
 // W tym przypadku CardProps mówi, że komponent Card musi otrzymać właściwość `value` typu string.
@@ -8,7 +9,23 @@ interface CardProps {
 }
 
 function Card({value} : CardProps) {
-  return <div className='card'>{value}</div>;
+  
+  const [flipped, setFlipped] = useState(false);
+
+  const handleClick = () => {
+    setFlipped(!flipped);
+  };
+
+  return (
+    // używamy shortif -> czyli jeżeli flipped jest prawdą -> klasa będzie się nazywała 'card flipped', jeżeli będzie false -> klasa będzie się nazywała 'card'
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator
+    <div className={`card ${flipped ? 'flipped' : ''}`} onClick={handleClick}>
+      <div className='card-inner'>
+        <div className='card-front'>?</div>
+        <div className='card-back'>{value}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
