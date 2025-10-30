40255780ba0d63ca13e1abaec09bd444b9631b0d
WKowalczykDev <wojciechkowalczyk10@gmail.com>
2025-10-30 16:15:24 +0100

    Dodanie przycisku resetowania i stałej czasu animacji

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ import { easyBoard, mediumBoard, hardBoard } from "./assets/boards";
 
 // funkcja tasowania tablicy - zmiana aby nie była posortowana
 function shuffleArray(array: string[]) {
-    const newArray = [];
-    const copy = [...array];
+  const newArray = [];
+  const copy = [...array];
 
-    while (copy.length > 0) {
-        const index = Math.floor(Math.random() * copy.length); 
-        newArray.push(copy[index]);
-        copy.splice(index, 1);
-    }
+  while (copy.length > 0) {
+    const index = Math.floor(Math.random() * copy.length);
+    newArray.push(copy[index]);
+    copy.splice(index, 1);
+  }
 
-    return newArray;
+  return newArray;
 }
 
 function App() {
@@ -23,24 +23,19 @@ function App() {
   const [level, setLevel] = useState<"easy" | "medium" | "hard">('easy');
   const [isGameChangePossible, setIsGameChangePossible] = useState(true);
 
+  const [newGameFlag, setNewGameFlag] = useState(false);
+
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setLevel(e.target.value as "easy" | "medium" | "hard");
     setCards(getBoard(e.target.value as "easy" | "medium" | "hard"));
   };
 
-  // ustawienie atrybutu disabled na select w zależności od stanu isGameChangePossible
-  // jeżeli isGameChangePossible jest false, select jest zablokowany
   useEffect(() => {
-    // pobieranie elementu select po id i nadajemy mu typ HTMLSelectElement lub null
     const select = document.getElementById("levelChangeSelect") as HTMLSelectElement | null;
-    // detektor, czy select istnieje, żeby uniknąć błędów
-    // jeżeli select istnieje, ustawiamy jego atrybut disabled na przeciwną wartość isGameChangePossible
     if (select) {
       select.disabled = !isGameChangePossible;
     }
-
-    // effect będzie reagował na zmiany isGameChangePossible
-  },[isGameChangePossible]);
+  }, [isGameChangePossible]);
 
   const getBoard = (level: "easy" | "medium" | "hard") => {
     let currentBoard;
@@ -53,6 +48,12 @@ function App() {
 
   const [cards, setCards] = useState<string[]>(getBoard(level));
 
+  useEffect(() => {
+    // TASOWANIE KART PRZY NOWEJ GRZE, PO RESECIE POZIOMU I WYKONANIU KODU W BOARD.TSX
+    if (!newGameFlag) {
+      setCards(getBoard(level));
+    }
+  }, [newGameFlag]);
 
   return (
     <div className="app">
@@ -66,7 +67,11 @@ function App() {
           </select>
         </label>
       </div>
-      <Board cards={cards} level={level} setIsGameChangePossible={setIsGameChangePossible} />
+      <Board cards={cards} level={level} setIsGameChangePossible={setIsGameChangePossible} newGameFlag={newGameFlag} setNewGameFlag={setNewGameFlag} />
+      {/* zmiana flagi nowej gry po kliknięciu przycisku Nowa Gra */}
+      <div className='newGame'>
+        <button onClick={() => { setNewGameFlag(true) }}>Nowa Gra</button>
+      </div>
     </div>
   );
 }
