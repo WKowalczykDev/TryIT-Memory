8d251750c76a69b4ed46d66c107318097d55a079
WKowalczykDev <wojciechkowalczyk10@gmail.com>
2025-10-31 00:01:00 +0100

    Obrazki <img> jako awers i rewers karty

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,6 @@
 import './../styles/Card.css'
+import { imageMap, colorMap } from '../assets/images';
+import { useEffect, useState } from 'react';
 
 interface CardProps {
   value: string;
@@ -6,14 +8,36 @@ interface CardProps {
   onClickToBoard: () => void;
 }
 
-function Card({value, flipped, onClickToBoard} : CardProps) {
+function Card({ value, flipped, onClickToBoard }: CardProps) {
 
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
+  // ustaw obrazek na podstawie value
+  useEffect(() => {
+    if (imageMap[value]) {
+      setImageSrc(imageMap[value]);
+    } else {
+      setImageSrc(null);
+    }
+  }, [value]);
+
+  const cardFront = imageMap['front'];
+  const cardColor = colorMap[value] || 'lightgray';
+  
   return (
     <div className={`card ${flipped ? 'flipped' : ''}`} onClick={onClickToBoard}>
       <div className='card-inner'>
-        <div className='card-front'>?</div>
-        <div className='card-back'>{value}</div>
+        <div className='card-front'>
+          <img src={cardFront} alt="Awers karty" />
+        </div>
+
+        <div className='card-back' style={{ backgroundColor: cardColor }}>
+          {imageSrc ? (
+            <img src={imageSrc} alt={`Karta ${value}`} className="card-image" />
+          ) : (
+            <span>{value}</span>
+          )}
+        </div>
       </div>
     </div>
   );
