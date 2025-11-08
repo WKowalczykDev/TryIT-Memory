398811b71914b509349f97acc81011b8ffa991f9
WKowalczykDev <wojciechkowalczyk10@gmail.com>
2025-11-08 15:18:04 +0100

    useEffect (reakcja na zmiany)

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,15 +1,24 @@
+import { useEffect } from "react";
+
 interface PlayerProps {
-  name: string;
-  score: number;
-  onAddPoint: () => void;
+    name: string;
+    score: number;
+    onAddPoint: () => void;
 }
 
 export default function Player({ name, score, onAddPoint }: PlayerProps) {
-  return (
-    <div style={{ border: "1px solid black", padding: "10px", margin: "5px" }}>
-      <h3>{name}</h3>
-      <p>Punkty: {score}</p>
-      <button onClick={onAddPoint}>Zdobądź punkt</button>
-    </div>
-  );
+    useEffect(() => {
+        console.log(`${name} ma teraz ${score} punktów.`);
+        if (score >= 10) {
+            alert(`${name} wygrał grę!`);
+        }
+    }, [score]);
+
+    return (
+        <div style={{ border: "1px solid black", padding: "10px", margin: "5px" }}>
+            <h3>{name}</h3>
+            <p>Punkty: {score}</p>
+            <button onClick={onAddPoint}>Zdobądź punkt</button>
+        </div>
+    );
 }
