13242183121b0c29f82d986e66c0a5a959448840
WKowalczykDev <wojciechkowalczyk10@gmail.com>
2025-11-08 14:11:26 +0100

    Wyswietlanie komponentow

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import './App.css';
+
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <div>
+      <h1>Moja pierwsza gra</h1>
+      <p>Witaj w React!</p>
+    </div>
+  );
 }
-
-export default App
+export default App;
