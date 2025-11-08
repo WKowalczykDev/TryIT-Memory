e538fdd9670bd200af930c48366a0fa8ba4c1629
WKowalczykDev <wojciechkowalczyk10@gmail.com>
2025-11-08 14:17:26 +0100

    Dzialanie useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [points, setPoints] = useState(0);
+
   return (
     <div>
-      <h1>Moja pierwsza gra</h1>
-      <p>Witaj w React!</p>
+      <h1>Punkty: {points}</h1>
+      <button onClick={() => setPoints(points + 1)}>+1</button>
+      <button onClick={() => setPoints(0)}>Reset</button>
     </div>
   );
 }
